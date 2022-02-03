4dbd1d2e169afeeffae1f772706056ba3c23e89c
Guido Belluomo <guidobelluomo@gmail.com>
2022-02-03 02:52:56 +0100

    Create ApproxSpring.js

---
+++ ApproxSpring.js
@@ -0,0 +1,57 @@
+function ApproxSpring(value, targetValue, strength, damping) {
+    let vel = 0;
+
+    this.calc = function (deltaTime) {
+        vel += (targetValue - value) * strength * deltaTime;
+        vel *= Math.pow(damping, deltaTime);
+        value += vel * deltaTime;
+    }
+
+    this.set = function (newValue, newTargetValue, newStrength, newDamping)
+    {
+        value = newValue
+        targetValue = newTargetValue
+        strength = newStrength
+        damping = newDamping
+    }
+
+    this.setValue = function (newValue)
+    {
+        value = newValue;
+    }
+
+    this.setTargetValue = function (newTargetValue)
+    {
+        targetValue = newTargetValue;
+    }
+
+    this.setStrength = function (newStrength)
+    {
+        strength = newStrength;
+    }
+
+    this.setDamping = function (newDamping)
+    {
+        damping = newDamping;
+    }
+
+    this.getValue = function ()
+    {
+        return value;
+    }
+
+    this.getTargetValue = function ()
+    {
+        return targetValue;
+    }
+
+    this.getStrength = function ()
+    {
+        return strength;
+    }
+
+    this.getDamping = function ()
+    {
+        return damping;
+    }
+}
